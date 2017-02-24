0fcb2a5173203cc3b202971218270ecf85a391ad
Priscilla <Priscillayg23@gmail.com>
2017-02-23 23:21:23 -0600

    added sql queries to db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,26 @@
+//create table for users info
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	username VARCHAR(200),
 	password VARCHAR(200)
-)
+);
+
+//create table for water intake info
+CREATE TABLE intake (
+	id SERIAL PRIMARY KEY,
+	date DATE,
+	time TIME,
+	volume INTEGER
+);
+
+// create table to link user_id to specific intake data
+CREATE TABLE users_intake (
+	users_id INTEGER NOT NULL REFERENCES users,
+	intake_id INTEGER NOT NULL REFERENCES intake
+);
+
+//test sql query to grab intake by day
+SELECT date, volume FROM intake WHERE date = '2017-02-23';
+
+//query to select date and volume from table and package by day
+SELECT date, SUM(volume) as total FROM intake GROUP BY date;
