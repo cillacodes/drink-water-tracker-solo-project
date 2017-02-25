f5534183323199e7d73c55e6f76baa6b1bca616b
Priscilla <Priscillayg23@gmail.com>
2017-02-24 21:55:01 -0600

    added nav menu

---
+++ public/scripts/config.js
@@ -0,0 +1,17 @@
+// angular.module('waterApp').controller('DailyController', function($http, $location, DailyService) {
+//   console.log('DailyController is loaded');
+//
+//    this.particle = function() {
+//      var deviceID = "2f0033000547343339373536",
+//       accessToken = "e91ff47d87b3de73e3bae77bb9c6d6d8ab1504dd",
+//       eventSource = new EventSource("https:api.particle.io/v1/devices/" + deviceID + "/events/?access_token=" + accessToken)
+//    };//end particle
+//
+//    this.particle();
+//
+//
+//
+//
+//
+//
+// });//end controller
--- public/scripts/daily.controller.js
+++ public/scripts/daily.controller.js
@@ -8,7 +8,7 @@ angular.module('waterApp').controller('DailyController', function($http, $locati
   ctrl.getIntake = function() {
     DailyService.getIntake().then(function(response) {
       console.log('Logging response inside getIntake', response);
-      
+
       ctrl.intakeList = response.data;
       console.log('intake list ', ctrl.intakeList);
       // console.log('response data', data);
@@ -18,20 +18,5 @@ angular.module('waterApp').controller('DailyController', function($http, $locati
   ctrl.getIntake();
 
 
-
-
-
-
-
-
-
-  // ctrl.getIntake = function() {
-  //   IntakeService.getIntake().then(function(response) {
-  //     console.log('Intake added', response);
-  //     ctrl.intakeList = response.data;
-  //     console.log('responsedata', response.data);
-  //   });
-  // };
-  // ctrl.getIntake();
 
 });//end controller
