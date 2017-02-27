a6dd85ffc12e734a364e30c2f817dd1e80a88766
Priscilla <Priscillayg23@gmail.com>
2017-02-26 21:03:53 -0600

    added particle-api code

--- public/scripts/daily.controller.js
+++ public/scripts/daily.controller.js
@@ -3,6 +3,11 @@ angular.module('waterApp').controller('DailyController', function($http, $locati
 
   var ctrl = this;
 
+  var objToSend = {
+    date: new Date(),
+    volume: 2
+  };
+
   ctrl.intakeList = [];
 
   ctrl.getIntake = function() {
@@ -11,12 +16,33 @@ angular.module('waterApp').controller('DailyController', function($http, $locati
 
       ctrl.intakeList = response.data;
       console.log('intake list ', ctrl.intakeList);
-      // console.log('response data', data);
     });
   };//end getIntake
 
   ctrl.getIntake();
 
+  var parsedData = null;
 
+  ctrl.particle = function() {
+    var deviceID = "2f0033000547343339373536",
+    accessToken = "e91ff47d87b3de73e3bae77bb9c6d6d8ab1504dd",
+    eventSource = new EventSource("https://api.particle.io/v1/devices/" + deviceID + "/events/?access_token=" + accessToken)
+
+    eventSource.addEventListener('button released', function(e) {
+       parsedData = JSON.parse(e.data);
+      console.log('Logging parsed data', parsedData);
+    });
+
+    ctrl.saveData = function(data) {
+        $http.post('/addIntake', data).then(function(response) {
+          console.log(response);
+          console.log('saved data posting', data);
+        });
+    };
+
+    ctrl.saveData(objToSend | limit: 1);
+  };//end particle
+
+  ctrl.particle();
 
 });//end controller
